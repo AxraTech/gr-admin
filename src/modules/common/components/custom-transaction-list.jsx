efed662303650f47e8ece9ab4b09b5a3f7424fa8
Saw Lin Thant <115479032+SawLinThant@users.noreply.github.com>
2024-09-04 16:24:55 +0700

    Staging (#8)
    * Add/authentication (#3)
    
    * add-login-and-register-form
    
    * add-authentication-remove-proxy
    
    * remove-password-add-localestring-change-row-purchase-cashin-color (#5)
    
    * add-home (#7)

---
+++ src/modules/common/components/custom-transaction-list.jsx
@@ -0,0 +1,54 @@
+import { useQuery } from "@apollo/client";
+import { GET_CARDS_TRANSACTION_SEVENDAYS } from "../../../graphql/query/card-transaction-query";
+import clsx from "clsx";
+
+const CustomTransctionList = () => {
+  
+
+  const { data: weeklyTransaction, loading: weeklyTransactionLoading } =
+    useQuery(GET_CARDS_TRANSACTION_SEVENDAYS);
+  const weeklyTransactionLists = weeklyTransaction
+    ? weeklyTransaction.card_transactions
+    : [];
+  if (weeklyTransactionLoading) return <div className="w-full h-full flex items-center justify-center">Loading...</div>;
+  return (
+    <>
+      {weeklyTransactionLists.length > 0 ? (
+        weeklyTransactionLists.map((transaction) => {
+            const colors = ["red", "blue", "green"];
+          const randomColor =colors[ Math.floor(Math.random() * colors.length)];
+          return (
+            <div
+              key={transaction.id}
+              className="w-full h-[5rem] bg-white border mt-2 border-gray-500 p-3 rounded grid grid-cols-3"
+            >
+              <div className="w-full h-full flex flex-row gap-2">
+                <div
+                  className={clsx("h-full w-2/5 border border-gray-400 rounded-lg",{
+                    "bg-blue-400": randomColor === 'blue',
+                    "bg-red-400": randomColor === 'red',
+                    "bg-green-400": randomColor === 'green'
+                  })}
+                ></div>
+                <div className="flex flex-col">
+                  <h2 className="font-bold">Card No</h2>
+                  <p className="font-light">{transaction.card.card_number}</p>
+                </div>
+              </div>
+              <div className="w-full h-full flex items-center justify-center">
+                <p>{transaction.amount.toLocaleString()} KS</p>
+              </div>
+              <div className="w-full h-full flex items-center justify-center">
+                <p>{transaction.card_transaction_type}</p>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <div>No transactions found.</div>
+      )}
+    </>
+  );
+};
+
+export default CustomTransctionList;

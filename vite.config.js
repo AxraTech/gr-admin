773fa9953f28684b5b867cf5a61c78a41942f50e
Saw Lin Thant <115479032+SawLinThant@users.noreply.github.com>
2024-09-04 16:23:58 +0700

    add-home (#7)

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // Expose to the network, accessible from other devices
+  },
 })

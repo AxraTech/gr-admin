b980b7bfd25b7fe062c81df06c9a8672c494b2e0
Saw Lin Thant <115479032+SawLinThant@users.noreply.github.com>
2024-09-13 12:20:46 +0700

    Hotfix/UI update (#25)
    * update-table-and-home
    
    * add-delete-function

--- src/pages/Dashboard/terminalDetail/[terminalId].jsx
+++ src/pages/Dashboard/terminalDetail/[terminalId].jsx
@@ -120,7 +120,7 @@ const TerminalDetail = () => {
             <div className="w-full h-full flex flex-col gap-4">
               <div className="w-full h-[4rem] flex flex-row items-center p-4 justify-between rounded-t rounded-tr bg-gradient-to-r from-blue-900 to-gray-600">
                 <button
-                  onClick={() => navigate("/dashboard/terminal")}
+                  onClick={() => navigate("/dashboard/terminal",{ state: { refetch: true } })}
                   className="bg-transparent"
                 >
                   <FaArrowLeft size={20} color="white" />
